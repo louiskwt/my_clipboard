5aead726264a5aa3fb13ef1b8ecadc3a851f7617
Ka Wing Tsang <kawingt2@gmail.com>
2021-03-06 18:49:09 +0800

    add UI class to refactor the code

--- script.js
+++ script.js
@@ -1,41 +1,90 @@
+// UI class to organise the UI template in one single place
+class UI {
+    constructor() {
+        this.body = document.querySelector('body');
+    }
+    buildBtn() {
+        const template = `
+                            <div id="open-clipboard">
+                                <span id="open-clipboard-text">Open</span>           
+                            </div>
+                        `;
+        this.body.innerHTML += template;
+        return document.querySelector('#open-clipboard');
+    }
+
+    buildClipboard() {
+        const template = `
+                            <div class="clipboard-modal">
+                                <div class="clipboard-header">
+                                    <div class="clipboard-title">Clipboard</div>
+                                    <button class="close-clipboard">&times;</button>
+                                </div>
+                                <div class="clipboard-body"></div>
+                            </div>
+                        `;
+        this.body.innerHTML += template;
+    }
+    buildOverlay() {
+        const template = `<div id="clipboard-overlay"></div>`
+        this.body.innerHTML += template;
+    }
+
+}
+// init UI class and build the UI
+const ui = new UI;
+
+ui.buildBtn();
+
+ui.buildClipboard();
+
+ui.buildOverlay();
+
+// get the UI elements for Eventlistener
+const openModalButton = document.querySelector('#open-clipboard');
+const modal = document.querySelector('.clipboard-modal');
+const overlay = document.querySelector('#clipboard-overlay');
+
+
 // get the body
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
-// UI for open clipboard
-const openModalButtons = document.createElement('div');
+// // UI for open clipboard
+// const openModalButtons = document.createElement('div');
 
-openModalButtons.innerHTML = `<span id="open-clipboard-text">Open</span>`;
-openModalButtons.id = 'open-clipboard'
+// openModalButtons.innerHTML = `<span id="open-clipboard-text">Open</span>`;
+// openModalButtons.id = 'open-clipboard'
 
-body.insertBefore(openModalButtons, body.firstChild);
+// body.insertBefore(openModalButtons, body.firstChild);
 
 
 // UI for modal
-const modal = document.createElement('div');
-modal.classList.add('clipboard-modal');
+// const modal = document.createElement('div');
+// modal.classList.add('clipboard-modal');
 
-const modalContent = `
-                        <div class="clipboard-header">
-                            <div class="clipboard-title">Clipboard</div>
-                            <button class="close-clipboard">&times;</button>
-                        </div>
-                        <div class="clipboard-body">
-                        </div>
-                    `
-modal.innerHTML += modalContent;
+// const modalContent = `
+//                         <div class="clipboard-header">
+//                             <div class="clipboard-title">Clipboard</div>
+//                             <button class="close-clipboard">&times;</button>
+//                         </div>
+//                         <div class="clipboard-body">
+//                         </div>
+//                     `
+// modal.innerHTML += modalContent;
 
-body.insertBefore(modal, body.secondChild);
+// body.insertBefore(modal, body.secondChild);
 
 // UI for overlay
-const overlay = document.createElement('div');
-overlay.id = 'clipboard-overlay';
+// const overlay = document.createElement('div');
+// overlay.id = 'clipboard-overlay';
 
-body.insertBefore(overlay, body.thirdChild);
+// body.insertBefore(overlay, body.thirdChild);
 
 
 
 // UI Eventlinstener
-openModalButtons.addEventListener('click', () => {
+openModalButton.addEventListener('click', () => {
+    console.log('fired');
     openModal(modal);
 })
 
@@ -54,6 +103,7 @@ overlay.addEventListener('click', () => {
 
 // Functions to open and close clipboard modal
 function openModal(modal) {
+    console.log('fired');
     if(modal == null) return
     modal.classList.add('active');
     overlay.classList.add('active');
@@ -64,19 +114,6 @@ function closeModal(modal) {
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
-
-
-// Show Alert UI
-function flashAlert(elem) {
-    const alertMsg = document.createElement('div');
-    alertMsg.classList.add('clipboard-alert');
-    alertMsg.textContent = 'Copied!';
-    elem.insertBefore(alertMsg, elem.firstChild);
-    setTimeout(() => {
-        document.querySelector('.clipboard-alert').remove();
-    }, 3000)
-}
-
 
 window.addEventListener('copy', (e) => {
     // Get the selected text
@@ -126,3 +163,14 @@ modal.addEventListener('click', (e) => {
         flashAlert(e.target.parentNode);
     } 
 })
+
+// Show Alert UI
+function flashAlert(elem) {
+    const alertMsg = document.createElement('div');
+    alertMsg.classList.add('clipboard-alert');
+    alertMsg.textContent = 'Copied!';
+    elem.insertBefore(alertMsg, elem.firstChild);
+    setTimeout(() => {
+        document.querySelector('.clipboard-alert').remove();
+    }, 3000)
+}
